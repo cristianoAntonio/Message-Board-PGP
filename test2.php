7b368021df2ec44fe3c15c1e462a4b42e18a4a71
Pedro Dariva <47556893+PedroDariva36@users.noreply.github.com>
2023-10-05 17:06:30 -0300

    Add files via upload

---
+++ test2.php
@@ -0,0 +1,56 @@
+<?php
+    //ini_set('display_errors', 1);
+    session_start();
+    include_once 'includes/db.php';
+    $sql = "select * from flag;"; 
+    $result = pg_query($conn, $sql);
+
+    $sql2 = "select * from board;"; 
+    $result2 = pg_query($conn, $sql2);
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/flag-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+
+    <title>Document</title>
+</head>
+<body>
+    <form action="includes/thread.php" enctype="multipart/form-data" method="post">
+        <label for="name">Title</label>
+        <input type="text" name="name" id="name">
+        <label for="context">Context</label>
+        <input type="text" name="context" id="context">
+        <select name="flag">
+            <?php while ($row = pg_fetch_row($result)) : ?>
+                <option value="<?=$row[0]?>">
+                    <?=$row[2]?>             
+                </option>
+            <?php endwhile;?>
+        </select>
+        <input type="hidden" name="MAX_FILE_SIZE" value="50000000" />
+        <input type="hidden" name="thread" value="<?=$_GET['thread']?>">
+        <input type="hidden" name="board" value="<?=$_GET['board']?>">
+        <input type="file" id="media" name="media"/>
+        <input type="submit" value="submit">
+    </form>
+
+    <form action="test2.php" method="get">
+        <select name="board">
+            <?php while ($row = pg_fetch_row($result2)) : ?>
+                <option value="<?=$row[0]?>">
+                    <?=$row[1]?>             
+                </option>
+            <?php endwhile;?>
+        </select>
+        <input type="submit" value="set board">
+    </form>
+
+</body>
+</html>
